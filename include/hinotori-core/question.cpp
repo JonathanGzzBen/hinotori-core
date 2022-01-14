e81f433e080d4711f074ff29af8cf574ddc0f870
JonathanGzzBen <jonathangzzben@gmail.com>
2022-01-13 20:27:00 -0600

    Add function to save questions in json file

--- include/hinotori-core/question.cpp
+++ include/hinotori-core/question.cpp
@@ -1,3 +1,57 @@
 #include "question.h"
 
 Question::Question(QObject *parent) : QObject{parent} {}
+
+Question::Question(QString message, qsizetype correctAnswerIndex,
+                   QStringList possibleAnswers, QObject *parent)
+    : message{message},
+      correctAnswerIndex{correctAnswerIndex},
+      possibleAnswers{possibleAnswers},
+      QObject{parent} {}
+
+bool Question::SaveQuestions(QList<QSharedPointer<Question>> &questions,
+                             QString filename) {
+  QFile saveFile(filename);
+  if (!saveFile.open(QIODevice::WriteOnly)) {
+    qWarning("Couldn't open save file.");
+    return false;
+  }
+
+  QJsonArray questionsArray;
+  for (const auto &question : questions) {
+    QJsonObject questionObj;
+    question->write(questionObj);
+    questionsArray.append(questionObj);
+  }
+
+  saveFile.write(QJsonDocument(questionsArray).toJson());
+  return true;
+}
+
+void Question::read(const QJsonObject &json) {
+  if (json.contains(kMessageJsonKey) && json[kMessageJsonKey].isString()) {
+    message = json[kMessageJsonKey].toString();
+  }
+  if (json.contains(kCorrectAnswerIndexJsonKey) &&
+      json[kCorrectAnswerIndexJsonKey].isDouble()) {
+    correctAnswerIndex = json[kCorrectAnswerIndexJsonKey].toInt();
+  }
+  if (json.contains(kPossibleAnswersJsonKey) &&
+      json[kPossibleAnswersJsonKey].isArray()) {
+    QJsonArray questionsArray = json[kPossibleAnswersJsonKey].toArray();
+    possibleAnswers.clear();
+    possibleAnswers.reserve(questionsArray.size());
+    for (int possibleAnswerIndex{0};
+         possibleAnswerIndex < possibleAnswers.size(); ++possibleAnswerIndex) {
+      possibleAnswers[possibleAnswerIndex] =
+          questionsArray[possibleAnswerIndex].toString();
+    }
+  }
+}
+
+void Question::write(QJsonObject &json) const {
+  json[kMessageJsonKey] = message;
+  json[kCorrectAnswerIndexJsonKey] = correctAnswerIndex;
+  QJsonArray possibleAnswersArray{QJsonArray::fromStringList(possibleAnswers)};
+  json[kPossibleAnswersJsonKey] = possibleAnswersArray;
+}
--- include/hinotori-core/question.h
+++ include/hinotori-core/question.h
@@ -1,8 +1,14 @@
 #ifndef QUESTION_H
 #define QUESTION_H
 
+#include <QFile>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
 #include <QObject>
+#include <QSharedPointer>
 #include <QString>
+#include <QStringList>
 
 #include "hinotori-core_global.h"
 
@@ -10,11 +16,22 @@ class HINOTORICORE_EXPORT Question : public QObject {
   Q_OBJECT
  public:
   explicit Question(QObject *parent = nullptr);
+  explicit Question(QString message, qsizetype correctAnswerIndex,
+                    QStringList possibleAnswers, QObject *parent = nullptr);
+  static bool SaveQuestions(QList<QSharedPointer<Question>> &questions,
+                            QString filename);
 
  private:
   QString message;
-  QList<QString> possibleAnswers;
   qsizetype correctAnswerIndex;
+  QStringList possibleAnswers;
+
+  const QString kMessageJsonKey = "message";
+  const QString kCorrectAnswerIndexJsonKey = "correctAnswerIndex";
+  const QString kPossibleAnswersJsonKey = "possibleAnswers";
+
+  void read(const QJsonObject &json);
+  void write(QJsonObject &json) const;
 
  signals:
 };
